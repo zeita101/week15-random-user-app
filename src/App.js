69d7a5d05910f0e5881b25a57b93e9e7d09cfdd7
Zipporah <zeitajb1@gmail.com>
2021-02-23 16:22:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import { fetchUser } from './services/rando_user';
 class App extends Component {
 
   state = {
-    currentUser: null
+    currentUser:
+    {
+      email: "",
+      name: {
+        title: "",
+        first: "",
+        last: "",
+      },
+      picture: {
+        medium: ""
+      }
+
+    }
   }
   // async/await
   async componentDidMount() {
@@ -18,7 +30,10 @@ class App extends Component {
   }
 
   refreshUser = async () => {
-
+    const value = await fetchUser()
+    console.log(value)
+    this.setState({ currentUser: value.data.results[0] })
+    console.log(value)
   }
 
 
@@ -27,13 +42,29 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Header">
-          <h1 style={{ color: "#CD5C5C" }}>Random User App 👩🏽 👨🏼 👶🏾</h1>
+          <h1 style={{ textAlign: 'center', color: "#CD5C5C" }}>Random User App 👩🏽 👨🏼 👶🏾</h1>
         </div>
         {/* must use .this when calling outside render() */}
-        <button onClick={this.refreshUser}>
-          Refresh
+        <div className="button">
+          <button style={{
+            backgroundColor: '#f08080', border: 'none', color: 'white', padding: '15px 32px', textAlign: 'center',
+            texDecoration: 'none', display: 'inlineBlock', fontSize: '18px', margin: '4px 2px', cursor: 'pointer'
+          }} onClick={this.refreshUser}>
+            Refresh
         </button>
+        </div>
 
+
+        <div className="display-user">
+          <img style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '10%' }} src={this.state.currentUser.picture.medium}></img>
+          <h2 style={{ textAlign: 'center' }}>
+            {this.state.currentUser.name.title}&nbsp;
+            {this.state.currentUser.name.first}&nbsp;
+            {this.state.currentUser.name.last}&nbsp;
+            </h2>
+          <h2 style={{ textAlign: 'center' }}>{this.state.currentUser.email}</h2>
+
+        </div>
 
       </div>
     );
